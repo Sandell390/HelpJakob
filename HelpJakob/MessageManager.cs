43d6c40d85fbbc35f70f8575892d4350f3210561
GommeS <sandell327@gmail.com>
2021-12-08 16:28:21 +0100

    Add project files.

---
+++ HelpJakob/MessageManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpJakob
+{
+    public abstract class MessageManager
+    {
+        private HTMLConverter htmlConverter = new HTMLConverter();
+        public virtual void SendMessage(Message m, bool isHTML)
+        {
+            if (isHTML)
+                m.Body = htmlConverter.ConvertBodyToHTML(m.Body);
+        }
+    }
+}
